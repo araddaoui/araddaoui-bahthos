61ed01987d219c04babb5b30693bb114f4bb2fff
araddaoui <6126381+araddaoui@users.noreply.github.com>
2026-08-14 00:24:13 +0000

    Resolve Vercel router imports for production TTS

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import ttsRouter from "../src/server/routers/tts";
-import chatRouter from "../src/server/routers/chat";
-import documentsRouter from "../src/server/routers/documents";
-import synthesisRouter from "../src/server/routers/synthesis";
-import reportFollowupRouter from "../src/server/routers/reportFollowup";
-import glossaryRouter from "../src/server/routers/glossary";
-import glossarySweepRouter from "../src/server/routers/glossarySweep";
+import ttsRouter from "../src/server/routers/tts.js";
+import chatRouter from "../src/server/routers/chat.js";
+import documentsRouter from "../src/server/routers/documents.js";
+import synthesisRouter from "../src/server/routers/synthesis.js";
+import reportFollowupRouter from "../src/server/routers/reportFollowup.js";
+import glossaryRouter from "../src/server/routers/glossary.js";
+import glossarySweepRouter from "../src/server/routers/glossarySweep.js";
 
 const app = express();
 
