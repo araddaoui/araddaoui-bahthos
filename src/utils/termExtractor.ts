04e4c0fa4541ecb3d444f125a29b807a84effc0e
araddaoui <araddaoui@gmail.com>
2026-09-18 11:53:56 -0600

    fix: add missing termExtractor utility file

---
+++ src/utils/termExtractor.ts
@@ -0,0 +1,10 @@
+﻿export function extractTerms(text: string): string[] {
+  if (!text) return [];
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s]/g, '')
+    .split(/\s+/)
+    .filter((word) => word.length > 2);
+}
+
+export default extractTerms;
